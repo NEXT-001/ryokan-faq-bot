f6d7e13d73bcdd06cdcaea4e5f077a80c6c81bc5
CSKen <kangju80@gmail.com>
2025-05-08 14:51:54 +0900

    t_confirm.py削除

--- t_confirm.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-df = pd.read_pickle("data/faq_with_embeddings.pkl")
-
-print(df.columns)  # ['question', 'answer', 'embedding']
-print(len(df["embedding"][0]))  # → 1536 であるべき
-print(type(df["embedding"][0][0]))  # floatであるべき
-
-
-
